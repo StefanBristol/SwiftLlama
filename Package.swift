3d715e18f124c39c3a115f4ded932f92f72e2a6d
Stefan Alsop <121760738+StefanBristol@users.noreply.github.com>
2025-04-03 15:44:26 +0100

    Redirected to sgml-org llama.cpp

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .library(name: "SwiftLlama", targets: ["SwiftLlama"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/stefanbristol/llama.cpp.git", branch: "master")
+        .package(url: "https://github.com/ggml-org/llama.cpp.git", branch: "master")
     ],
     targets: [
         .target(name: "SwiftLlama", 
